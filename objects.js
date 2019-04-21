fb4f5361e35f3c7a93110f94cd2fb342652978cf
Ervin Csala <csala.ervin@gmail.com>
2019-04-21 14:29:18 +0200

    solarSystemAdded

---
+++ objects.js
@@ -0,0 +1,28 @@
+/*jslint devel: true */
+var alert;
+
+var bestSong = {
+  'title': 'Gangsta\'s Paradise',
+  'author': 'Coolio',
+  'genre': 'Rap'
+};
+alert(bestSong);
+
+var myBook = {
+  'title': 'Ne bántsátok a feketerigót!',
+  'author': 'Harper Lee',
+  'genre': 'fejlődésregény'
+};
+alert(myBook);
+
+var favorites = {
+  'color': 'green',
+  'number': 6,
+  'foods': ['rice', 'beef', 'egg', 'fish']
+};
+alert(favorites);
+
+myBook.rating = 5;
+
+var solarSystem = {'Mercury': 'planet', 'Venus': 'planet', 'Earth': 'planet', 'Mars': 'planet', 'Jupiter': 'planet', 'Saturn': 'planet', 'Uranus': 'planet', 'Neptune': 'planet', 'Pluto': 'planet', 'Ceres': 'dwarf planet', 'Makemake': 'dwarf planet', 'Haumea': 'dwarf planet', 'Eris': 'dwarf planet'};
+alert(solarSystem);
